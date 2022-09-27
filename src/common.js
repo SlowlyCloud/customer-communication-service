099f0340eb8e08b5f17dcabbf545a6127117611c
rick ryan <rick.ryan2@outlook.com>
2022-09-27 18:50:04 +0000

    feat: email notificaiton

---
+++ src/common.js
@@ -0,0 +1,8 @@
+module.exports.Meta = class Meta {
+    constructor() {
+        this.createdAt = new Date()
+        this.updatedAt = null
+        this.deletedAt = null
+        this.version = 0
+    }
+}
