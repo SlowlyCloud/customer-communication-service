14c8653a605fdee368711f0ccd56ae03725ebe25
rick ryan <rick.ryan2@outlook.com>
2022-10-19 17:45:03 +0000

    feat: logging debug info every request

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('express-async-errors')
+const log = require('./logging')
 const config = require('./config')
 const { getFiles } = require('./common')
 const app = express()
@@ -9,14 +10,36 @@ const apisFolderName = 'apis'
 const port = config.server.port
 const basePath = config.server.basePath
 
+app.use((req, res, next) => {
+  log.debug({
+    direction: 'inbound',
+    req: req
+  })
+  res.on("finish", () => log.debug({
+    direction: 'outbound',
+    res: res
+  }))
+  next()
+})
+
 // load & register all routers by their relative path
 var path = require('path').join(__dirname, apisFolderName)
 getFiles(path).forEach(file => {
   let apiPath = file.replace(path, '').replace('.js', '')
-  console.log(`loading file:${file} & registering path of route ${apiPath}`);
+  log.info(`loading file:${file} & registering path of route ${apiPath}`)
   app.use(basePath + apiPath, require(file))
 })
 
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).send(err.message)
+  log.error({
+    err: err,
+    req: req,
+    res, res
+  })
+  next()
+})
+
 app.listen(port, () => {
-  console.log(`Customer Communication Service Started on Port ${port} with Base Path ${basePath}`)
+  log.info(`Customer Communication Service Started on Port ${port} with Base Path ${basePath}`)
 })
---
+++ src/logging.js
@@ -0,0 +1,11 @@
+const config = require('./config')
+const bunyan = require('bunyan')
+const logger = bunyan.createLogger({
+  name: "css",
+  level: config.server.loggingLevel,
+  serializers: bunyan.stdSerializers
+})
+
+logger.info('Logger created, level: %s', logger.level())
+
+module.exports = logger
