2a8f2169a3bd7634e9be2c172375c53311f18b28
Rick Ryan <rick.ryan2@outlook.com>
2022-10-23 01:42:42 +0800

    Feat/authorization middleware (#17)
    * rebase to fix:tg-webhook
    
    * feat: authorization middleware
    
    * feat: authorization middleware for specificed router

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 const express = require('express')
 require('express-async-errors')
 const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
 const log = require('./logging')
 const config = require('./config')
-const { getFiles } = require('./common')
+const { getDirs } = require('./common')
 const app = express()
 
 // config
 const apisFolderName = 'apis'
 const port = config.server.port
 const basePath = config.server.basePath
+const kepPem = Buffer.from(config.server.auth.privateKey, 'base64')
 
 app.use((req, res, next) => {
   log.debug({
@@ -23,14 +25,35 @@ app.use((req, res, next) => {
   next()
 })
 
+app.get("/token", (req, res) => {
+  res.send(
+    jwt.sign(
+      { key: 'value' },
+      kepPem,
+      {
+        algorithm: config.server.auth.algorithm,
+        expiresIn: 30
+      }
+    )
+  )
+})
+
 app.use(bodyParser.json())
 
 // load & register all routers by their relative path
 var path = require('path').join(__dirname, apisFolderName)
-getFiles(path).forEach(file => {
-  let apiPath = file.replace(path, '').replace('.js', '')
-  log.info(`loading file:${file} & registering path of route ${apiPath}`)
-  app.use(basePath + apiPath, require(file))
+log.info('start loading router from path: %s ...', path)
+getDirs(path, '2', 1).filter(v => /.*\/v[0-9]{1}$/.test(v)).forEach(moduleDir => {
+  let apiPath = basePath + moduleDir.replace(path, '')
+  log.info('module: %s loading...', moduleDir)
+  app.use(apiPath, require(moduleDir))
+  log.info('registered path of router: %s', apiPath)
+})
+getDirs(path, '1', 1).forEach(file => {
+  let apiPath = basePath + file.replace(path, '').replace('.js', '')
+  log.info('file: %s loading...', file)
+  app.use(apiPath, require(file))
+  log.info('registered path of router: %s', apiPath)
 })
 
 app.use((err, req, res, next) => {
